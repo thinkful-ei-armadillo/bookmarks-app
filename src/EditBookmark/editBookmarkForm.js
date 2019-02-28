fa65630c990090d3bce16d090c103997f4229427
Hunter Kreshock <hkreshock@gmail.com>
2019-02-28 15:07:01 -0700

    commit-

---
+++ src/EditBookmark/editBookmarkForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from  'react';
+
+export default class EditArticleForm extends Component {
+  /* state for inputs etc... */
+  render() {
+    return (
+      <section className='EditArticleForm'>
+        <h2>Edit article</h2>
+        <form>
+          {/* inputs etc... */}
+        </form>
+      </section>
+    )
+  }
+}
